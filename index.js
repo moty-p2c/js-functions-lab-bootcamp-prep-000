8b3ef6666901cee5ec9c44e673f1117cb79edd07
moty pitusy <moty.p2c@gmail.com>
2017-12-25 18:48:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 
-function 
+function happyHolidayTo(holiday,name){
+  return `happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}`;
+}
